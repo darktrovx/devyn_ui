3999ed3fb1c39a4f1c2e374762ca722dc3f5b96a
Devyn <mage1d@yahoo.com>
2024-08-08 10:16:13 -0400

    fix: export names

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,16 @@
 
 
 RegisterCommand('test1', function()
-    exports.ui:createInfoItem('test1', 'Test 1', 'This is a test')
+    exports.devyn_ui:createInfoItem('test1', 'Test 1', 'This is a test')
+    exports.devyn_ui:createInfoItem('test2', 'Test 2', 'This is a test')
+    exports.devyn_ui:createInfoItem('test3', 'Test 3', 'This is a test')
+    exports.devyn_ui:createInfoItem('test4', 'Test 4', 'This is a test')
 end)
 
 RegisterCommand('test2', function()
-    exports.ui:removeInfoItem('test1')
+    exports.devyn_ui:removeInfoItem('test1')
 end)
 
 RegisterCommand('test3', function()
-    exports.ui:createInfoItem('test3', 'Test 3', 'This is a test 3')
+    exports.devyn_ui:createInfoItem('test3', 'Test 3', 'This is a test 3')
 end)
